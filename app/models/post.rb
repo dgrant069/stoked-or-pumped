5fda135a204be439304cebf463cb9aea103323a5
Dylan Grant <dgrant069@hotmail.com>
2013-06-30 21:38:26 -0700

    First commit. Posts and comments working

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ActiveRecord::Base
+  attr_accessible :content, :datestamp, :mediaurl, :name, :title
+
+  validates :name, :presence => true
+  validates :title, :presence => true,
+  					:length => { :minimum => 3 }
+
+  has_many :comments
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StokedOrPumped::Application.config.secret_token = 'c8d7f80447b3441b43464447f8a3ab6fde9be6242ca4931ea2684ab206afdb2766c15db7e3445211afca5351f7b900c3d11bf725f678c7ce75d636b488d4148a'
